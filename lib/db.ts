a4066502433aa8a6620c2d946038f5aa748aa704
arc <arcmaximizer@proton.me>
2026-02-19 13:13:30 +0000

    refactor: remove legacy db setup

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely } from "kysely";
-export { Database as Sqlite } from "@db/sqlite";
-import { DenoSqlite3Dialect } from "@soapbox/kysely-deno-sqlite";
-
-const db = new Kysely({
-  dialect: new DenoSqlite3Dialect({
-    database: new Sqlite("db.sqlite3"),
-  }),
-});
