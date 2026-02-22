e6b9bfade8432872705246b4530a26927df1452a
arc <arcmaximizer@proton.me>
2026-02-22 22:01:30 +0000

    feat: dummy runner

---
+++ svc/runner.ts
@@ -0,0 +1,21 @@
+// Runner module handles running a given event and returning a transaction
+// as well as any state changes and so on
+
+import type { Event, TransactionId } from "./events.ts";
+
+class Runner {
+  constructor() {}
+  spawn() {
+  }
+  async executeRoot(id: TransactionId, root: Event): Result<Transaction, Error> {
+    // execute a root event
+    
+    return {
+      id,
+      root,
+      diffs: [],
+      inputs: [],
+      effects: []
+    }
+  }
+}
