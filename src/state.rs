2d02c6320030c1750df4525476e25c0e50c08a80
arc <arcmaximizer@proton.me>
2026-05-08 17:59:01 +0000

    refactor: transform state into actor model

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,45 @@
 use crate::types::ProcessId;
 use std::collections::HashMap;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+pub enum StateMsg {
+    Set {
+        process: ProcessId,
+        key: String,
+        value: String,
+        resp: tokio::sync::oneshot::Sender<()>,
+    },
+    Get {
+        process: ProcessId,
+        key: String,
+        resp: tokio::sync::oneshot::Sender<Option<String>>,
+    },
+}
+
+pub async fn run_state(mut rx: mpsc::UnboundedReceiver<StateMsg>, mut state: InMemoryKVState) {
+    while let Some(msg) = rx.recv().await {
+        match msg {
+            StateMsg::Set {
+                process,
+                key,
+                value,
+                resp,
+            } => {
+                state.set(&process, &key, value);
+                let _ = resp.send(());
+            }
+            StateMsg::Get {
+                process,
+                key,
+                resp,
+            } => {
+                let value = state.get(&process, &key);
+                let _ = resp.send(value);
+            }
+        }
+    }
+}
 
 pub trait KVState {
     fn set(&mut self, process: &ProcessId, key: &str, value: String);
